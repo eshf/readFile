0fb3e85f79aa54f011713e095b3e6a211eec663c
eshf <62289445+eshf@users.noreply.github.com>
2022-10-19 01:21:10 +0800

    Update readFile.java

--- readFile.java
+++ readFile.java
@@ -1,5 +1,3 @@
-package readFile.src;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -11,7 +9,7 @@ public class readFile {
     public static void main(String[] args) {
         if (args.length < 3 || args.length > 3) {
             System.out.println("Parameters input by user is incorrect.");
-			return;
+            return;
         }
 
         else {
@@ -23,57 +21,42 @@ public class readFile {
     }
 
     private static void fileOutput(String outputZipFile) {
-         String outpath = outdir + "/" + entry.getName();
-            FileOutputStream output = null;
-            try
-            {
-                output = new FileOutputStream(outpath);
-                int len = 0;
-                while ((len = stream.read(buffer)) > 0)
-                {
-                    output.write(buffer, 0, len);
-                }
+        String outpath = outdir + "/" + entry.getName();
+        FileOutputStream output = null;
+        try {
+            output = new FileOutputStream(outpath);
+            int len = 0;
+            while ((len = stream.read(buffer)) > 0) {
+                output.write(buffer, 0, len);
             }
-            finally
-            {
-                // we must always close the output file
-                if(output!=null) output.close();
-            }
-			stream.close();
-			return outputFile;
-			System.out.println("File is exported successfully.");
-			
+        } finally {
+            // we must always close the output file
+            if (output != null)
+                output.close();
+        }
+        stream.close();
+        System.out.println("File is exported successfully.");
+
+    }
+
+    private static void readZippedFile(String inZip) {
+        byte[] buffer = new byte[2048];
+
+        ZipEntry entry;
+        while ((entry = stream.getNextEntry()) != null) {
+            String s = String.format("Entry: %s len %d added %TD", entry.getName(), entry.getSize(),
+                    new Date(entry.getTime()));
+            System.out.println(s);
         }
 
-        
-    
+    }
 
-private static void readZippedFile(String inZip){
- byte[] buffer = new byte[2048];
- 
- 
-		ZipEntry entry;
-        while((entry = stream.getNextEntry())!=null)
-        {
-            String s = String.format("Entry: %s len %d added %TD",
-                            entry.getName(), entry.getSize(),
-                            new Date(entry.getTime()));
-            System.out.println(s);
-		}
-		
-}
-	
-	private static String result(String inFile, String fileOutput){
-		
-		String outputFile="";
-		
-		outputFile= readFile.readZippedFile(inFile);
-		
-		char[] msgarr = output.toCharArray();
+    private static void result(String inFile, String outFile) {
 
-        readFile.fileOutput(msgarr, fileOutput);
-		
-		
-	}
+        readFile.readZippedFile(inFile);
+
+        readFile.fileOutput(outFile);
+
+    }
 
 }
