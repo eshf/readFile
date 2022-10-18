fda1e2c4ceddd0c213051d7fca801ecb3ddaa189
eshf <62289445+eshf@users.noreply.github.com>
2022-10-19 01:16:32 +0800

    Add files via upload
    Helper Class for reading txt files and output file in current directory.

---
+++ readFile.java
@@ -0,0 +1,79 @@
+package readFile.src;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class readFile {
+    public static void main(String[] args) {
+        if (args.length < 3 || args.length > 3) {
+            System.out.println("Parameters input by user is incorrect.");
+			return;
+        }
+
+        else {
+            if (args[0].equalsIgnoreCase("-r")) {
+                readFile.result(args[1], args[2]);
+            }
+        }
+
+    }
+
+    private static void fileOutput(String outputZipFile) {
+         String outpath = outdir + "/" + entry.getName();
+            FileOutputStream output = null;
+            try
+            {
+                output = new FileOutputStream(outpath);
+                int len = 0;
+                while ((len = stream.read(buffer)) > 0)
+                {
+                    output.write(buffer, 0, len);
+                }
+            }
+            finally
+            {
+                // we must always close the output file
+                if(output!=null) output.close();
+            }
+			stream.close();
+			return outputFile;
+			System.out.println("File is exported successfully.");
+			
+        }
+
+        
+    
+
+private static void readZippedFile(String inZip){
+ byte[] buffer = new byte[2048];
+ 
+ 
+		ZipEntry entry;
+        while((entry = stream.getNextEntry())!=null)
+        {
+            String s = String.format("Entry: %s len %d added %TD",
+                            entry.getName(), entry.getSize(),
+                            new Date(entry.getTime()));
+            System.out.println(s);
+		}
+		
+}
+	
+	private static String result(String inFile, String fileOutput){
+		
+		String outputFile="";
+		
+		outputFile= readFile.readZippedFile(inFile);
+		
+		char[] msgarr = output.toCharArray();
+
+        readFile.fileOutput(msgarr, fileOutput);
+		
+		
+	}
+
+}
